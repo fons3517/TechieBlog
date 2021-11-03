ae5c71cd59a43f7738a1571d279bf39d1fb85681
Alfonso Robles <fons3517@gmail.com>
2021-11-03 17:12:41 -0500

    required homeRoutes in index.js

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
 const postRoutes = require('./postRoutes');
+const homeRoutes = require('../homeRoutes');
 
 router.use('/users', userRoutes);
-router.use('/projects', postRoutes);
+router.use('/posts', postRoutes);
+router.use('../homeRoutes', homeRoutes);
 
 module.exports = router;
