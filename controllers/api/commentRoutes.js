c75218cde8908df4f2637e5aa6c2a23de3601532
Alfonso Robles <fons3517@gmail.com>
2021-12-01 14:30:01 -0600

    completed routes in commentRoutes.js

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -2,10 +2,20 @@ const router = require('express').Router();
 const { Comment } = require('../../models');
 const withAuth = require('../../utils/auth');
 
+router.get('/', async (req, res) => {
+    try {
+        await Comment.findAll()
+        res.status(200).json(CommentData);
+    } catch (err) {
+        console.log("Error", err)
+        res.status(500).json(err);
+    }
+});
 router.post('/', withAuth, async (req, res) => {
     try {
         const newComment = await Comment.create({
-            ...req.body,
+            comment_text: req.body.comment_text,
+            post_id: req.body.post_id,
             user_id: req.session.user_id,
         });
 
@@ -28,7 +38,6 @@ router.delete('/:id', withAuth, async (req, res) => {
             res.status(404).json({ message: 'No post found with this id!' });
             return;
         }
-
         res.status(200).json(commentData);
     } catch (err) {
         res.status(500).json(err);
