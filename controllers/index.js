50118d0fdfcafd49decdfbb9b099ff98cb43bc58
Alfonso Robles <fons3517@gmail.com>
2021-12-01 14:33:16 -0600

    added all routes to index.js

--- controllers/index.js
+++ controllers/index.js
@@ -1,9 +1,13 @@
 const router = require('express').Router();
-
+const userRoutes = require('./userRoutes');
 const apiRoutes = require('./api');
 const homeRoutes = require('./homeRoutes');
+const commentRoutes = require('./comment-routes');
 
 router.use('/', homeRoutes);
 router.use('/api', apiRoutes);
+router.use('/users', userRoutes);
+router.use('/posts', postRoutes);
+router.use('/comments', commentRoutes);
 
 module.exports = router;
